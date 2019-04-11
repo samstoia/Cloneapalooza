2c6b2e37af212e823c37f0003fd424d27325ca8a
samstoia <samstoia@gmail.com>
2019-04-11 08:13:28 -0700

    refactor api call to not return lollapalooza value

--- src/app/lineup/lineup.component.ts
+++ src/app/lineup/lineup.component.ts
@@ -11,6 +11,7 @@ export class LineupComponent implements OnInit {
 
   artists;
   names: any[];
+  ignoreName: any[];
 
   constructor(private data: ApiService) { }
 
@@ -18,10 +19,17 @@ export class LineupComponent implements OnInit {
     this.data.getArtists().subscribe(data => {
       this.artists = data;
       this.names = [];
+      this.ignoreName = [];
       let arr = this.artists._embedded.events[0]._embedded.attractions;
       for (let i=0; i<arr.length; i++) {
-        let artistName: string = arr[i].name;
-        this.names.push(artistName);
+        if (arr[i].name === 'Lollapalooza') {
+          let ignoreValue: string = arr[i].name;
+          this.ignoreName.push(ignoreValue);
+        } else {
+          let artistName: string = arr[i].name;
+          this.names.push(artistName);
+        }
+        
       }
     })
   }
